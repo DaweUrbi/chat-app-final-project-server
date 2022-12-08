dad1fde43b3789fc7405954e43c18372944428b8
DaweUrbi <urban.da@icloud.com>
2022-12-08 12:01:15 -0800

    server deploy

---
+++ controllers/usersController.js
@@ -0,0 +1,122 @@
+const User = require("../model/userModel");
+const bcrypt = require("bcrypt");
+
+module.exports.register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    const emailCheck = await User.findOne({ email });
+
+    if (usernameCheck) {
+      return res.json({ msg: "Username already used", status: false });
+    }
+    if (emailCheck) {
+      return res.json({ msg: "Email already used", status: false });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    delete user.password;
+    req.session.user = user;
+    return res.json({ status: true, user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.json({ msg: "Incorrect username or password", status: false });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.json({ msg: "Incorrect username or password", status: false });
+    }
+
+    delete user.password;
+    req.session.user = user;
+    console.log(req.session)
+    return res.json({ status: true, user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.setAvatar = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(
+      userId,
+      {
+        isAvatarImageSet: true,
+        avatarImage,
+      },
+      { new: true }
+    );
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.params.id } }).select([
+      "email",
+      "username",
+      "avatarImage",
+      "_id",
+    ]);
+    return res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.logout = async (req, res, next) => {
+  try {
+    req.session = null;
+    return res.json({ status: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.checkLogin = async (req, res, next) => {
+  try {
+    console.log(req.session)
+    if (req.session.user) {
+      return res.json({ status: true, user: req.session.user });
+    }
+    return res.json({ status: false });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.setUser = async (req, res, next) => {
+  const { user } = req.body;
+  try{
+    if(req.session.user){
+      req.session.user = user;
+      return res.json({status: true});
+    }
+  }catch(error){
+    console.log(error);
+  }
+};
---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const cookieSession = require("cookie-session");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messagesRoutes");
+
+const socket = require("socket.io");
+
+const app = express();
+
+require("dotenv").config();
+
+app.use(
+  cors({
+    origin: "https://daweurbi.github.io",
+    credentials: true,
+    methods: ["GET", "POST", "DELETE"],
+  })
+);
+
+app.set("trust proxy", 1);
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+    secure: true,
+    sameSite: "none",
+    httpOnly: false,
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', "https://daweurbi.github.io");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
+
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: process.env.ORIGIN,
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+let users = {};
+
+io.on("connection", (socket) => {
+  socket.on("add-user", (userId) => {
+    users[userId] = socket.id;
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = users[data.to];
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("receive-message", data);
+    }
+  });
+});
